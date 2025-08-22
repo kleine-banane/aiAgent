1d1496b23c7d3d706372fdfa0ecaff4b2236961d
kleine-banane <kleine-banane@outlook.de>
2025-08-22 22:00:04 +0200

    prompt können an gemini geschickt werden. --verbose ist möglich

---
+++ main.py
@@ -0,0 +1,39 @@
+# to activate virtual environment: source .venv/bin/activate
+# to run: uv run main.py
+
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=get_prompt())]),
+    ]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print(response.text)
+    verbose(response)
+
+def get_prompt():
+    return sys.argv[1]
+
+def verbose(response):
+    try:
+        if sys.argv[1] and sys.argv[2] == "--verbose":
+            print(f"User prompt: {get_prompt()}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except IndexError:
+        pass
+
+if __name__ == "__main__":
+    main()
