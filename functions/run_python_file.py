54f67a5708d17bf6feaf92c2a00705d8e7bd1aa9
kleine-banane <kleine-banane@outlook.de>
2025-08-30 22:05:57 +0200

    joa2

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -6,3 +6,9 @@ def run_python_file(working_directory, file_path, args=[]):
     
     if not target_path.startswith(abs_working_dir):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(target_path):
+        return f'Error: File "{file_path}" not found.'
+    if not target_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    
